93d4ebecf81e5017be9d3dec7e50a0ddbc4360f6
PatrickBett <patrickbett018@gmail.com>
2023-09-19 19:43:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution in this file!
+const employee={name:'Sam',streetAddress:'12 Broadway'};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return (employee.name,employee.streetAddress);
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){}
+function functdeleteFromEmployeeByKey(employee, key){}
+function destructivelyDeleteFromEmployeeByKey(employee, key){}
+
